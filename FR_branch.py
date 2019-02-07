1761a48aafd88fb07f9cca14954f19d6c3c14c44
Guo, Dian <dianguo@gatech.edu>
2019-02-06 21:18:25 -0500

    face_detect update

--- FR_branch.py
+++ FR_branch.py
@@ -1,30 +1,37 @@
 import numpy as np
 import cv2
-import RepeatedTimer
 
 class FacialRecognition():
     def __init__(self):
-        total_images = 10
+        pass
 
     def parse_frame(self, frame):
         pass
 
-    def face_detect(total_images, frame):
-        
+    def face_detect(self, frame):
+        """
+        Get the faces in frame
+
+        try different cascades params
+
+        :param frame: image
+        :return: a list of images of faces
+        """
+        roi_color_faces = []
         # Multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
         face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-        
         for (x,y,w,h) in faces:
-            roi_color_face = frame[y:y+h, x:x+w]
-            # writes image out
-            # cv2.imwrite("face-" + str(index) + ".png", roi_color_face)
+            roi_color_faces.append(frame[y:y+h, x:x+w])
+        return roi_color_faces
             
         
 if __name__ == '__main__':
     fr = FacialRecognition()
-    for i in range():        
-        rt = RepeatedTimer.RepeatedTimer(1,face_detect(1, i))
-
+    faces = fr.face_detect(cv2.imread("faceTest.jpg")) 
+    i = 0   
+    for face in faces:
+        cv2.imwrite("face{}".format(i) + ".jpg", face)
+        i += 1
+        
