5187b0e5aacec1fd7ac4a8616f555bde2e128134
Samuel Gedaly Guenoun <samuelgedaly@MacBook-Pro-de-Gisela.local>
2019-03-13 13:10:09 -0400

    Added Face++ integration

--- FE_branch.py
+++ FE_branch.py
@@ -1,9 +1,72 @@
 import numpy
+import urllib3
+import urllib
+import time
+import cv2
+from datetime import datetime, timedelta
+import base64
+import json
+import requests
 
 
-class FacialExtraction():
+class FeatureExtraction():
     def __init__(self):
-        pass
+        self.fpp = Facepp()
+        self.frame_counter = 1
+        self.start_time = self.now()
+        self.delta = timedelta(seconds = 1.2)
+        self.block = False
+
+    def now(self):
+        return datetime.now()
 
     def parse_frame(self, frame):
-        pass
+        if self.now() - self.start_time > self.delta and not self.block:
+            self.block = True
+            cv2.imwrite('./mmegg.jpeg', frame)
+
+            try:
+                data = self.fpp.parse_frame('./mmegg.jpeg')
+                print (data)
+
+            except Exception as e:
+                print (e)
+
+            self.start_time = self.now()
+            self.block = False
+
+       
+
+class Facepp():
+    def __init__(self):
+        self.http_url = 'https://api-us.faceplusplus.com/facepp/v3/detect'
+        self.key = "LX2oRHchELnqIswcEGQMCpMslc05lE7m"
+        self.secret = "4sK29DKX4gJrJ-wv9l4RQyR0VlQQS9Mw"
+        self.http = urllib3.PoolManager()
+
+    def getBaseFormat(self):
+        data_dict = {}
+        
+        data_dict['api_key'] = self.key
+        data_dict['return_attributes'] = "gender,age,ethnicity"
+        data_dict['api_secret'] = self.secret
+
+        return data_dict
+
+    def parse_frame(self, path):
+        querystring = self.getBaseFormat()
+        
+        files = {
+        'image_file': ('whatever.jpg', open(path, 'rb')),
+        'Content-Type':'image/jpeg',
+        }
+        
+        try:
+            #post data to server
+            resp = requests.request("POST", self.http_url, files=files, params=querystring)
+            qrcont=resp.text
+            jason = json.loads(qrcont)
+            
+            return jason['faces']
+        except Exception as e:
+            print (e)
