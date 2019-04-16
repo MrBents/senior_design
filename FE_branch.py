de0436b74d9cd7d5abaceb9c2629c46184f6eaf4
Max Bentata <maxbentata@Maxs-MacBook-Pro-4.local>
2019-04-16 13:38:14 -0400

    forcing push

--- FE_branch.py
+++ FE_branch.py
@@ -7,66 +7,75 @@ from datetime import datetime, timedelta
 import base64
 import json
 import requests
+import os, os.path
 
 
-class FeatureExtraction():
+class FeatureExtraction:
     def __init__(self):
         self.fpp = Facepp()
         self.frame_counter = 1
         self.start_time = self.now()
-        self.delta = timedelta(seconds = 1.2)
+        self.delta = timedelta(seconds=1.2)
         self.block = False
+        self.counter = len(
+            [name for name in os.listdir("./imgs/") if os.path.isfile(name)]
+        )
 
     def now(self):
         return datetime.now()
 
     def parse_frame(self, frame):
-        if self.now() - self.start_time > self.delta and not self.block:
-            self.block = True
-            cv2.imwrite('./mmegg.jpeg', frame)
+        # if self.now() - self.start_time > self.delta and not self.block:
+        self.block = True
+        self.counter += 1
+        path = "./imgs/mmegg " + str(self.counter) + ".jpeg"
+        cv2.imwrite(path, frame)
 
-            try:
-                data = self.fpp.parse_frame('./mmegg.jpeg')
-                print (data)
+        try:
+            data = self.fpp.parse_frame(path)
+            print(data)
+            return data
 
-            except Exception as e:
-                print (e)
+        except Exception as e:
+            print(e)
 
-            self.start_time = self.now()
-            self.block = False
+        self.start_time = self.now()
+        self.block = False
 
-       
 
-class Facepp():
+class Facepp:
     def __init__(self):
-        self.http_url = 'https://api-us.faceplusplus.com/facepp/v3/detect'
+        self.http_url = "https://api-us.faceplusplus.com/facepp/v3/detect"
         self.key = "LX2oRHchELnqIswcEGQMCpMslc05lE7m"
         self.secret = "4sK29DKX4gJrJ-wv9l4RQyR0VlQQS9Mw"
         self.http = urllib3.PoolManager()
 
     def getBaseFormat(self):
         data_dict = {}
-        
-        data_dict['api_key'] = self.key
-        data_dict['return_attributes'] = "gender,age,ethnicity"
-        data_dict['api_secret'] = self.secret
+
+        data_dict["api_key"] = self.key
+        data_dict["return_attributes"] = "gender,age,ethnicity"
+        data_dict["api_secret"] = self.secret
 
         return data_dict
 
     def parse_frame(self, path):
         querystring = self.getBaseFormat()
-        
+
         files = {
-        'image_file': ('whatever.jpg', open(path, 'rb')),
-        'Content-Type':'image/jpeg',
+            "image_file": ("whatever.jpg", open(path, "rb")),
+            "Content-Type": "image/jpeg",
         }
-        
+
         try:
-            #post data to server
-            resp = requests.request("POST", self.http_url, files=files, params=querystring)
-            qrcont=resp.text
+            # post data to server
+            resp = requests.request(
+                "POST", self.http_url, files=files, params=querystring
+            )
+            qrcont = resp.text
             jason = json.loads(qrcont)
-            
-            return jason['faces']
+            print(jason)
+            return jason["faces"]
         except Exception as e:
-            print (e)
+            print(e)
+
